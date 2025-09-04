c1e00fed5a246eb5981bfdc8389f806c3f328d8a
KarthikBS-6 <bskarthik058@gmail.com>
2025-09-04 19:20:33 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -6,7 +6,7 @@ csv_file = 'cpu_memory.csv'
 
 def log_metrics():
     cpu = psutil.cpu_percent(interval=1)
-    memory = psutil.virtual_memory.percent
+    memory = psutil.virtual_memory().percent
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     with open(csv_file, 'a', newline='') as f:
